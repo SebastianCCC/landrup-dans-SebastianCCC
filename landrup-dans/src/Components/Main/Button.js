410b65c29f1ccd6b9b4e1271655f99bf709e0319
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-10-04 15:54:06 +0200

    add Aktivitetsdetalje Page, initial commit

--- landrup-dans/src/Components/Main/Button.js
+++ landrup-dans/src/Components/Main/Button.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const Button = ({ title, link }) => {
   return (
-    <Link className="w-[60%]" to={link}>
+    <Link className="w-[65%]" to={link}>
       <button className="button">{title}</button>
     </Link>
   )
