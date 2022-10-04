d1384c10ed199c47050b2339a331fcd472689eb0
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-10-04 17:00:58 +0200

    add Sog Page, initial commit

---
+++ landrup-dans/src/Components/Main/Assets/index.js
@@ -0,0 +1,13 @@
+export const SogIcon = () => {
+  return (
+    <svg width={24} height={24} fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M11 19a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM21 21l-4.35-4.35"
+        stroke="#EAEAEA"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
