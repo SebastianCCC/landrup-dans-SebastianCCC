0f561f12ada2fbcff006fe023ab9a143d56ec45b
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-10-04 11:46:40 +0200

    add Tab, initial commit

---
+++ landrup-dans/src/Components/Header/Assets/index.js
@@ -0,0 +1,51 @@
+export const HomeIcon = () => {
+  return (
+    <svg width={41} height={41} fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="m12 16 9-7 9 7v11a2 2 0 0 1-2 2H14a2 2 0 0 1-2-2V16Z"
+        stroke="#000"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18 29V19h6v10"
+        stroke="#000"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <circle cx={20.5} cy={20.5} r={20} stroke="#000" />
+    </svg>
+  )
+}
+
+export const SogIcon = () => {
+  return (
+    <svg width={41} height={41} fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx={20.5} cy={20.5} r={20} stroke="#000" />
+      <path
+        d="M19 27a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM29 29l-4.35-4.35"
+        stroke="#000"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export const kalenderIcon = () => {
+  return (
+    <svg width={41} height={41} fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx={20.5} cy={20.5} r={20} stroke="#000" />
+      <path
+        d="M28 13H14a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V15a2 2 0 0 0-2-2ZM25 11v4M17 11v4M12 19h18"
+        stroke="#000"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
