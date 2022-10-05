7efcd0471abaa9b4ce4188c3b4b192f28adeca77
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-10-05 13:18:49 +0200

    add Kanleder Page, initial commit

--- landrup-dans/src/Pages/Kalender.js
+++ landrup-dans/src/Pages/Kalender.js
@@ -1,5 +1,50 @@
+import { useState, useContext, useEffect } from 'react'
+import KalenderKort from '../Components/Main/KalenderKort'
+import LandrupApiUser from '../Hooks/LandrupApiUser'
+import AktivitetApi from '../Hooks/AktivitetApi'
+import { StateContext } from '../Util/StateContext'
+
 const Kalender = () => {
-  return <h1>Kalender</h1>
+  const { user } = useContext(StateContext)
+  const [filteredKalender, setFilteredKalender] = useState([])
+  const { aktiviteter } = AktivitetApi({})
+  const { userData } = LandrupApiUser({ id: user.userId, token: user.token })
+  console.log(filteredKalender)
+
+  useEffect(() => {
+    setFilteredKalender(
+      aktiviteter &&
+        aktiviteter.filter((singleAktivitet) => singleAktivitet.instructorId === user.userId)
+    )
+  }, [aktiviteter])
+  return (
+    <>
+      {filteredKalender?.length || userData?.activities?.length ? (
+        <section className="flex flex-col">
+          {user.role == 'instructor' &&
+            filteredKalender.map(({ name, weekday, time, id }, i) => {
+              return (
+                <KalenderKort
+                  key={i}
+                  title={name}
+                  decs={`${weekday} ${time}`}
+                  id={id}
+                  role={user.role}
+                />
+              )
+            })}
+          {user.role == 'default' &&
+            userData.activities.map(({ name, weekday, time, id }, i) => {
+              return <KalenderKort key={i} title={name} decs={`${weekday} ${time}`} id={id} />
+            })}
+        </section>
+      ) : (
+        <h2 className="text-sm font-normal text-white">
+          Det ser ud til du ikke har nogen planer...
+        </h2>
+      )}
+    </>
+  )
 }
 
 export default Kalender
